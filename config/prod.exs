5fbdc819c8e2a4be227c44a299fd20bcd57f0a9b
Héctor Alonso Salinas García <ha_sg@outlook.com>
2025-09-08 11:13:28 -0600

    Set prod url

--- config/prod.exs
+++ config/prod.exs
@@ -1,5 +1,5 @@
 import Config
 
-config :tableau, :config, url: "https://example.com"
+config :tableau, :config, url: "https://z7ealth.dev"
 config :tableau, Tableau.PostExtension, future: false, dir: ["_posts"]
 config :tableau, Tableau.PageExtension, dir: ["_pages"]
