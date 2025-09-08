19eba4181e60d69c37037ebca9cddbcc2a3a3d3d
Héctor Alonso Salinas García <ha_sg@outlook.com>
2025-09-08 11:23:49 -0600

    Added mix task to build website into /docs

---
+++ lib/mix/tasks/build.ex
@@ -0,0 +1,23 @@
+defmodule Mix.Tasks.Z7ealthWebsite.Build do
+  use Mix.Task
+
+  @shortdoc "Build website"
+  @moduledoc @shortdoc
+
+  @doc false
+  def run(_argv) do
+
+    source =
+      "./_site"
+
+    destination = "./docs"
+
+    if File.exists?(destination) do
+      File.rmdir!(destination)
+    end
+
+    Mix.Task.run("tableau.build")
+
+    File.cp_r(source, destination)
+  end
+end
